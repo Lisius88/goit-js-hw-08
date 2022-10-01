70f8ded67df782d958a3c7aeb914c7dc0822cc43
Makarov Yelisei <fenniz123@gmail.com>
2022-10-01 12:13:53 +0300

    haj-tak

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+///////////////////////////////////////////Пермененные\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+// /////////////////////////////////////////Шаблон и Рендер\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+const galleryTemplate = ({ preview, original, description }) =>
+  `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+
+const render = () => {
+  const list = galleryItems.map(galleryTemplate).join('');
+
+  refs.gallery.innerHTML = '';
+  refs.gallery.insertAdjacentHTML('beforeend', list);
+};
+render();
+
+////////////////////////////////////////////////////////////Сімпл-Лайтбокс\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const currentTimeKey = 'videoplayer-current-time';
+console.log(player);
+
+player.on('timeupdate', throttle(writeCurrentTime, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem(currentTimeKey))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
+
+function writeCurrentTime(data) {
+  localStorage.setItem(currentTimeKey, data.seconds);
+}
