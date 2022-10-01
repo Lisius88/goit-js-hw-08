a94970c116f97061ec2765f4ce66b9b9948f9371
Makarov Yelisei <fenniz123@gmail.com>
2022-10-01 18:05:47 +0300

    final

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,15 +3,16 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 const currentTimeKey = 'videoplayer-current-time';
-console.log(player);
 
-player.on('timeupdate', throttle(writeCurrentTime, 1000));
+const onPlay = function (data) {
+  localStorage.setItem(currentTimeKey, data.seconds);
+};
+
+player.on('blabla', onPlay);
 
 player
   .setCurrentTime(localStorage.getItem(currentTimeKey))
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
+  .then(function (seconds) {})
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
@@ -20,7 +21,3 @@ player
         break;
     }
   });
-
-function writeCurrentTime(data) {
-  localStorage.setItem(currentTimeKey, data.seconds);
-}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,46 @@
-console.log(typeof null);
-console.log('hello');
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('form');
+const keyForStorage = 'feedback-form-state';
+let feedbackFormData = {};
+
+populateTextArea();
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onInput(evt) {
+  feedbackFormData[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(keyForStorage, JSON.stringify(feedbackFormData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const storedFormDataObject = JSON.parse(localStorage.getItem(keyForStorage));
+  if (storedFormDataObject) {
+    console.log(storedFormDataObject);
+  }
+
+  resetLocalStorage(evt);
+}
+
+function populateTextArea() {
+  const saveMessage = JSON.parse(localStorage.getItem(keyForStorage));
+
+  if (saveMessage?.email) {
+    form.email.value = saveMessage.email;
+  }
+  if (saveMessage?.message) {
+    form.message.value = saveMessage.message;
+  }
+}
+
+function resetLocalStorage(evt) {
+  localStorage.removeItem(keyForStorage);
+
+  evt.target.reset();
+
+  feedbackFormData = {};
+}
