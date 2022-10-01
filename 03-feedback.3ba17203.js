eb8433e75fcada34f228de598c8f2ce7653ce825
Lisius88 <104981367+Lisius88@users.noreply.github.com>
2022-10-01 09:14:13 +0000

    Deploying to gh-pages from @ Lisius88/goit-js-hw-08@70f8ded67df782d958a3c7aeb914c7dc0822cc43 🚀

---
+++ 03-feedback.3ba17203.js
@@ -0,0 +1,2 @@
+console.log("object"),console.log("hello");
+//# sourceMappingURL=03-feedback.3ba17203.js.map
---
+++ 03-feedback.bde62836.js
@@ -0,0 +1,2 @@
+console.log("object"),console.log("hello");
+//# sourceMappingURL=03-feedback.bde62836.js.map
